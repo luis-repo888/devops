8c4d8f366746cc67dc0ee5309226ce666126830e
likui <likui@yovole.com>
2026-01-26 15:48:40 +0800

    update

--- inference/docker_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-docker run --gpus all --privileged --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -it -v /data:/data --network host -v /dev/infiniband:/dev/infiniband --security-opt=seccomp=unconfined  --rm nvcr.io/nvidia/sglang:25.12-py3
